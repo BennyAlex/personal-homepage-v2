271aa5c7d6939319b40d16ae8705eb8096baadba
Benjamin Franz <benny.franz@eye-able.com>
2023-11-21 21:34:00 +0100

    Add docs for build output

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    outDir: 'docs'
+  }
 })
