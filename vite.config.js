54532fdff2cac45394b166956746429a8c3c8dec
Benjamin Franz <benny.franz@eye-able.com>
2023-11-21 21:49:28 +0100

    Add base config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  base: '/personal-homepage-v2/',
   build: {
     outDir: 'docs'
   }
